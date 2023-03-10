db40b80db441f5553c757307c4d54c7f1b079aa4
Izzy <ishtar.eliasen@gmail.com>
2023-03-09 21:08:18 -0800

    Added a utils file and a (non functional) ui generation script

---
+++ site/js/UI.js
@@ -0,0 +1,14 @@
+// The purpose of this file is to generate the UI overlaying the arcgis map
+// The map itself is created in Map.js, not here
+
+// Get the ui div from the page
+
+
+function generateOrRegenerateUI(uiWrapper) {
+    // Clear the existing UI
+    uiWrapper.innerHTML = "";
+}
+
+
+
+generateOrRegenerateUI(document.getElementById("UI"));
