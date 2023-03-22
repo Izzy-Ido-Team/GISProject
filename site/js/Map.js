d5cefc1b397cef0972d0ad8986a86f6af10976c2
ishoshani <idoshoshani@gmail.com>
2023-03-21 21:06:43 -0700

    circles that cover points

--- site/js/Map.js
+++ site/js/Map.js
@@ -1,23 +1,24 @@
-require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/CSVLayer", "esri/widgets/TimeSlider"], function(esriConfig, Map, MapView, CSVLayer, TimeSlider) {
+require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/CSVLayer", "esri/layers/GraphicsLayer", "esri/widgets/TimeSlider", "esri/geometry/Circle", "esri/Graphic"], function (esriConfig, Map, MapView, CSVLayer, GraphicsLayer, TimeSlider, Circle, Graphic) {
     esriConfig.apiKey = "AAPKf31ae4a2e344423d8600437097e2d8a5QjPJWfZfJUHAPBUSJlUOInVwGVj725rxdzL_nu58NQgn4lYm53ivCRF2ONMOGF-a"
     const url =
-    "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.csv";
+        "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.csv";
 
     var map = new Map({
         basemap: "topo-vector"
     });
+    var nlayer = undefined;
 
     const renderer = {
         type: "heatmap",
         colorStops: [
-          { color: "rgba(63, 40, 102, 0)", ratio: 0 },
-          { color: "#472b77", ratio: 0.083 },
+            { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+            { color: "#472b77", ratio: 0.083 },
 
-          { color: "#ffff00", ratio: 1 }
+            { color: "#ffff00", ratio: 1 }
         ],
         maxDensity: 0.01,
         minDensity: 0
-      };
+    };
 
     var view = new MapView({
         container: "map",
@@ -30,19 +31,20 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/CSVLayer"
         view: view,
         mode: "time-window",
         fullTimeExtent: {
-          start: new Date().setDate(new Date().getDate() - 7),
-          end: new Date()
+            start: new Date().setDate(new Date().getDate() - 7),
+            end: new Date()
         },
-        timeExtent:{ 
+        timeExtent: {
             start: new Date().setDate(new Date().getDate() - 7),
             end: new Date()
         },
         stops: {
-          interval: {
-            value: 6,
-            unit: "hours"
-          }
-        }});
+            interval: {
+                value: 6,
+                unit: "hours"
+            }
+        }
+    });
 
     view.ui.add(timeSlider, "bottom-right");
 
@@ -59,16 +61,16 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/CSVLayer"
     timeSlider.watch("timeExtent", function (value) {
         updateDefinitionExpression(value);
     });
-  
+
     function updateDefinitionExpression(timeExtent) {
-      const start = timeStampFormat(timeExtent.start);
-      const end = timeStampFormat(timeExtent.end);
-      csvLayer.definitionExpression = `time >= TIMESTAMP '${start}' AND time <= TIMESTAMP '${end}'`;
-      console.log(csvLayer.definitionExpression)
-    
+        const start = timeStampFormat(timeExtent.start);
+        const end = timeStampFormat(timeExtent.end);
+        csvLayer.definitionExpression = `time >= TIMESTAMP '${start}' AND time <= TIMESTAMP '${end}'`;
+        console.log(csvLayer.definitionExpression)
+
     }
     // return date in format yyyy-mm-dd hh:mm:ss
-    function timeStampFormat(date){
+    function timeStampFormat(date) {
         const year = date.getFullYear();
         const month = date.getMonth() + 1;
         const day = date.getDate();
@@ -79,9 +81,89 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/CSVLayer"
     };
 
 
-    document.getElementById('addLayer').addEventListener('click', function() {
+    const process = new Processor(1, 'days', 1000, url, 'url');
+    document.getElementById('addLayer').addEventListener('click', function () {
         map.layers.add(csvLayer);
     });
+    document.getElementById('addDifferenceLayer').addEventListener('click', function () {
+        if(nlayer){
+            map.layers.remove(nlayer);
+        }
+        const test1 = new Date().setDate(new Date().getDate() - 7);
+        const test2 = new Date();
+
+        const differences = process.generateDifference(test1, test2);
+        
+        const graphicCircles = []
+        differences.normalizedLosses.forEach((circlePoint, index) => {
+            const center = circlePoint.center1;
+            const loss = circlePoint.loss;
+            const shape = new Circle({
+                center: [center.x, center.y],
+                geodesic: true,
+                numberOfPoints: 100,
+                radius: 1000,
+                radiusUnit: 'kilometers'
+            });
+
+            graphicCircles.push( new Graphic({
+                geometry: shape,
+                symbol: {
+                    type: "simple-fill",
+                    style: "solid",
+                    color: [227, 139, 79, (loss * 0.9)],
+                    outline: {
+                        color: 'red',
+                        width: 1
+                    }
+                },
+                attributes: {
+                    oid: index,
+                    loss: loss
+                }
+            }));
+        });
+
+        differences.normalizedGain.forEach((circlePoint, index) => {
+            const center = circlePoint.center2;
+            const gain = circlePoint.gain;
+            const shape = new Circle({
+                center: [center.x, center.y],
+                geodesic: true,
+                numberOfPoints: 100,
+                radius: 1000,
+                radiusUnit: 'kilometers'
+            });
+
+            graphicCircles.push( new Graphic({
+                geometry: shape,
+                symbol: {
+                    type: "simple-fill",
+                    style: "solid",
+                    color: [79, 139, 244, (gain*0.9)],
+                    outline: {
+                        color: 'blue',
+                        width: 1
+                    }
+                },
+                attributes: {
+                    oid: index,
+                    gain: gain
+                }
+            }));
+        });
+
+        
 
 
-});
+        console.log(graphicCircles)
+        nlayer = new GraphicsLayer({
+                graphics: graphicCircles
+            });
+        map.layers.add(nlayer);
+
+
+        });
+
+
+    });
--- site/js/Process.js
+++ site/js/Process.js
@@ -1,78 +1,88 @@
+const earthRadiusKm = 6371;
 
 class Processor {
     timeRange;
     timeUnit;
     maxRadius;
-    constructor(timeRange, timeUnit, maxRadius) {
+    src;
+    srcType
+    constructor(timeRange, timeUnit, maxRadius, src, srcType) {
         this.timeRange = timeRange;
         this.timeUnit = timeUnit;
         this.maxRadius = maxRadius
+        this.src = src;
+        this.srcType = srcType;
+        if(srcType === 'url'){
+            fetch(src).then((response) => {
+                return response.text();
+            }).then((text) => {
+                this.src = text;
+            });
+        }
     }
 
     //Generate the difference GeoJSON
-    generateDifference(Csv, time1, time2) {
-        const timePoints1 = filterTimesWithinM(this.convertCSVtoArray(Csv), time1, this.timeRange, this.timeUnit);
-        const timePoints2 = filterTimesWithinM(this.convertCSVtoArray(Csv), time2, this.timeRange, this.timeUnit);
+    generateDifference(time1, time2) {
+        const srcAsArray = this.convertCSVtoArray(this.src);
+        const timePoints1 = filterTimesWithinM(srcAsArray, time1, this.timeRange, this.timeUnit);
+        const timePoints2 = filterTimesWithinM(srcAsArray, time2, this.timeRange, this.timeUnit);
         //find the central points of the time1;
-        const {centers1,count1} = this.findPointCenters(timePoints1);
+        const result1 = this.findPointCenters(timePoints1);
+        const {centers: centers1, counts: count1} = result1;
         //check how many points are within these circles in time2
-        const {centers2, count2}= centers1.map((center,index)=>{
-            const pointsWithin = timePoints2.filter((point)=>{
-                return getDistance(point,center) <= this.maxRadius;
-            })
-            return {center,pointsWithin,points1:points1[index]};
-        })
-
-        const negativeFeatureSet = [];
-        const positiveFeatureSet = [];
-
-        //For each circle center, add a number of points equal to the difference between the timepoints
-        //adding to the correct feature set
-        centers1.forEach((center, index) => {
-            if(count1[index] > count2[index]){
-                for(let i = 0; i < count1[index] - count2[index]; i++){
-                    negativeFeatureSet.push({
-                        geometry: {
-                            type: "point",
-                            longitude: center.x,
-                            latitude: center.y
-                        }
-                    });
-                }
-            }else if(count1[index] < count2[index]){
-                for(let i = 0; i < count2[index] - count1[index]; i++){
-                    positiveFeatureSet.push({
-                        geometry: {
-                            type: "point",
-                            longitude: center.x,
-                            latitude: center.y
-                        }
-                    });
-                }
-            }
+        const result2= this.findPointCenters(timePoints2);
+        const {centers: centers2, counts: count2} = result2;
+        //find loss in centers1
+        const lossInCenters = centers1.map((center1, index) => {
+               const localCount1 = count1[index];
+               const localCount2 = this.findLocalPointsAtTime(center1, timePoints2).length;
+               const loss = localCount1 - localCount2;
+               return {center1, loss};
+        }).filter(lossPoint => lossPoint.loss>0); // get rid of negative values
+        let highestLoss = Math.max(...lossInCenters.map((lossPoint) => lossPoint.loss));
+        if(highestLoss == 0){
+            highestLoss = 1; //avoid divide by zero
+        }
+        const normalizedLosses = lossInCenters.map((lossPoint) => {
+            const {center1, loss} = lossPoint;
+            return {center1, loss: loss / highestLoss};
         });
-        const negativeGeoJSON = {
-            type: "FeatureCollection",
-            features: negativeFeatureSet
-        };
-        const positiveGeoJSON = {
-            type: "FeatureCollection",
-            features: positiveFeatureSet
-        };
-        return {negativeGeoJSON, positiveGeoJSON};
+        //find gain in centers2
+        const gainInCenters = centers2.map((center2, index) => {
+            const localCount1 = this.findLocalPointsAtTime(center2, timePoints1).length;
+            const localCount2 = count2[index];
+            const gain = localCount2 - localCount1;
+            return {center2, gain};
+        }).filter(gainPoint => (gainPoint.gain>0));// only include areas with gain
+        let highestGain = Math.max(...gainInCenters.map((gainPoint) => gainPoint.gain));
+        if(highestGain == 0){
+            highestGain = 1; //avoid divide by zero
+        }
+        const normalizedGain = gainInCenters.map((gainPoint) => {
+            const {center2, gain} = gainPoint;
+            return {center2, gain: gain / highestGain};
+        });
+        
+        //create the feature circles for example            
+        
+        return {normalizedLosses, normalizedGain};
 
     }
 
     findPointCenters(timePoints) {
-        const convertToCoordinates = timePoints.map((point) => {
-            return { x: point.lat, y: point.long };
+        return findCircleCenters(timePoints, this.maxRadius);
+    }
+
+    findLocalPointsAtTime(center, timePoints){
+        return timePoints.filter((point) => {
+            const distance = getDistanceInKm(center.y, center.x, point.y, point.x);
+            return distance <= this.maxRadius;
         });
-        return this.findCircleCenters(convertToCoordinates, this.maxRadius);
     }
 
     //convert a csv into an array of objects with the attributes {time, lat, long, mag}
-    convertCSVtoArray(Csv) {
-        const lines = Csv.split("\n");
+    convertCSVtoArray(src) {
+        const lines = src.split("\n");
         const result = [];
         const headers = lines[0].split(",");
         for (let i = 1; i < lines.length; i++) {
@@ -81,6 +91,8 @@ class Processor {
             for (let j = 0; j < headers.length; j++) {
                 obj[headers[j]] = currentline[j];
             }
+            obj.x=Number.parseFloat(obj['longitude']);
+            obj.y=Number.parseFloat(obj['latitude']);
             result.push(obj);
         }
         return result;
@@ -126,9 +138,7 @@ function findCircleCenters(points, maxRadius) {
 
 // Helper function to calculate distance between two points
 function getDistance(point1, point2) {
-    const dx = point1.x - point2.x;
-    const dy = point1.y - point2.y;
-    return Math.sqrt(dx * dx + dy * dy);
+    return getDistanceInKm(point1.y, point1.x, point2.y, point2.x);
 }
 
 // Helper function to calculate center of a circle given its points
@@ -171,4 +181,28 @@ function filterTimesWithinM(points, n, m, unit) {
         return isWithinM;
     });
 }
+function degreeToRad(deg){
+    return deg * (Math.PI/180);
+  }
+
+
+function getDistanceInKm(lat1, lon1, lat2, lon2){
+    const dLat = degreeToRad(lat2 - lat1);
+    
+    // Calculate the longitudinal distance, accounting for the wrap-around
+    const dLon = Math.abs(degreeToRad(lon2 - lon1));
+    const lonWrap = (dLon > Math.PI) ? 2*Math.PI - dLon : dLon;
+  
+    // Use the haversine formula to calculate the great-circle distance
+    const a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+              Math.cos(degreeToRad(lat1)) * Math.cos(degreeToRad(lat2)) *
+              Math.sin(lonWrap/2) * Math.sin(lonWrap/2);
+  
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  
+    const distance = earthRadiusKm * c;
+  
+    return distance;
+};  
+  
 
